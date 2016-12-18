08c913ee6646d7d171bde84e52302955a4e58cad
radu567 <filipesco@mail.ru>
2016-12-18 11:37:53 +0200

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,6 +1,7 @@
 import psycopg2
 
 
+# conexiune la baza de date : nume baza de date, utilizator, parola, host, portul pe care se afla baza de date
 conn = psycopg2.connect(database="db", user="postgres", password="1111", host="127.0.0.1", port="5432")
 print("Conexiune realizata cu succes")
 
