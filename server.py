c24645c2a9650f09ef8dfa2a6ae4137db90aab5c
Gherman Olga <gherman.olya@gmail.com>
2017-01-05 23:33:33 +0200

    retrieve data from database

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
-from bottle import route, run
+from bottle.bottle import route, run
+import psycopg2
 
-@route('/hello')
-def hello():
-    return "Hello World!"
+@route('/index')
+def index():
+    conn = psycopg2.connect(database="postgres_db", user="postgres", password="1", host="127.0.0.1", port="5432")
+    if conn.closed != 0:
+        return "Can't connect to database"
+    else:
+        curs = conn.cursor()
+        curs.execute("select * from student order by id asc;")
+        rows = curs.fetchall()
+    conn.close()
+    return str(rows)
 
 run(host='localhost', port=8080, debug=True)
