597e733da406a57774f7db037e11cee3ae86cfe5
Gherman Olga <gherman.olya@gmail.com>
2017-01-06 15:05:39 +0200

    changes port for server and writes explicitly method get

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from bottle.bottle import route, run, template
 import psycopg2
 
-@route('/index')
+@route('/index', method='GET')
 def index():
     conn = psycopg2.connect(database="postgres_db", user="postgres", password="1", host="127.0.0.1", port="5432")
     if conn.closed != 0:
@@ -13,4 +13,4 @@ def index():
     conn.close()
     return template('index', result=rows)
 
-run(host='localhost', port=8080, debug=True)
+run(host='localhost', port=8888, debug=True)
