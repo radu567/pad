a0709d0d36a33880941bcbb7d7780d7cf05c8328
Gherman Olga <gherman.olya@gmail.com>
2017-01-24 16:29:31 +0200

    retrives data from db and return json

--- server.py
+++ server.py
@@ -1,16 +1,71 @@
-from bottle.bottle import route, run, template
-import psycopg2
+from __future__ import unicode_literals
+from bottle.bottle import route, run, template, error, redirect
+from sqlalchemy import (
+    Column, String, Integer, Sequence,
+    create_engine, MetaData
+)
+import json
+from models import Student
 
-@route('/index', method='GET')
-def index():
-    conn = psycopg2.connect(database="postgres_db", user="postgres", password="1", host="127.0.0.1", port="5432")
-    if conn.closed != 0:
-        return "Can't connect to database"
-    else:
-        curs = conn.cursor()
-        curs.execute("select * from student order by id asc;")
-        rows = curs.fetchall()
-    conn.close()
-    return template('index', result=rows)
+from sqlalchemy.orm import sessionmaker, class_mapper
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+def get_connection_string(user, password, db, host='localhost', port=5432):
+    '''
+    Receives configuration params for the connection url, returns database connection string
+    '''
+    connection_string = 'postgresql://{username}:{password}@{host}:{port}/{db}'
+    return connection_string.format(
+        username=user,
+        password=password,
+        host=host,
+        port=port,
+        db=db,
+    )
+
+def query(db, table, order=False, criterion=False):
+    '''
+    Queries objects from the DB and prints retrieved objects
+    '''
+    result = db.query(Student).order_by(Student.name)
+    for student in result:
+        print('{} - {}'.format(student.name, student.mark))
+    return result
+
+def get_model(engine, table_name):
+  meta = MetaData()
+  meta.reflect(bind=engine)
+  table = meta.tables[table_name]
+  return table
+
+def serialize(model):
+  """Transforms a model into a dictionary which can be dumped to JSON."""
+  # first we get the names of all the columns on model
+  columns = [c.key for c in class_mapper(model.__class__).columns]
+  # then we return their values in a dict
+  return dict((c, getattr(model, c)) for c in columns)
+
+@route('/index/<table_name>', method='GET')
+def index(table_name):
+    db_url = get_connection_string('postgres', '1', 'postgres')
+    engine = create_engine(db_url, echo=True) # Set echo=False if you don't want output from the database driver in the console
+    session_factory = sessionmaker(bind=engine)
+    db_session = session_factory()
+    serialized_data = [
+      serialize(row)
+      for row in query(db=db_session, table=get_model(engine, table_name))
+    ]
+    json_obj = json.dumps(serialized_data)
+    return json_obj
+
+@route('/index')
+def wrong():
+    redirect("/index/student")
+
+@error(404)
+def error404(error):
+    return 'Page not found'
 
 run(host='localhost', port=8888, debug=True)
