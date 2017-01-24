a2fda845645bf990c8f0c04c504e7962b2118150
Gherman Olga <gherman.olya@gmail.com>
2017-01-24 23:21:30 +0200

    tries to redirect client request to datawarehouse

--- proxy/routes.py
+++ proxy/routes.py
@@ -1,5 +1,4 @@
 from .views import index
 
-
 def setup_routes(app):
-    app.router.add_get('/', index)
+    app.router.add_get('/student', index)
--- proxy/views.py
+++ proxy/views.py
@@ -1,5 +1,30 @@
 from aiohttp import web
+import asyncio, aiohttp
 
+import sys
 
 async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+    url = str(request.rel_url)
+    response = await method(url)
+    return web.Response(text="text")
+
+
+
+async def fetch(session, url):
+    with aiohttp.Timeout(10):
+        response = await session.get(url)
+        try:
+            return (await response.text())
+        finally:
+            if sys.exc_info()[0] is not None:
+                response.close()
+            else:
+                await response.release()
+
+async def method(url):
+    loop = asyncio.get_event_loop()
+    with aiohttp.ClientSession(loop=loop) as session:
+        html = loop.run_until_complete(
+            fetch(session, 'http://localhost:8888/student'))
+        print(html)
+        return "response"
