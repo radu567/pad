98fb6ff6be1d2dada5b36e6374e8201aa8dd2839
radu567 <filipesco@mail.ru>
2017-01-26 10:19:26 +0200

    Update views.py
    Adds Redis , set, get, and time to live for cache : view_cache(url)

--- proxy/views.py
+++ proxy/views.py
@@ -1,25 +1,27 @@
 from aiohttp import web
-import asyncio, aiohttp
-
+import asyncio
+import aiohttp
+import redis
 import sys
+
 
 async def index(request):
     url = str(request.rel_url)
-    response = await method(url)
-    return web.Response(text="text")
-
+    result = view_cache(url)
+    return web.Response(text=result)
 
 
 async def fetch(session, url):
     with aiohttp.Timeout(10):
         response = await session.get(url)
         try:
-            return (await response.text())
+            return await response.text()
         finally:
             if sys.exc_info()[0] is not None:
                 response.close()
             else:
                 await response.release()
+
 
 async def method(url):
     loop = asyncio.get_event_loop()
@@ -28,3 +30,14 @@ async def method(url):
             fetch(session, 'http://localhost:8888/student'))
         print(html)
         return "response"
+
+    
+async def view_cache(url):
+    redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
+    if redis_db.get('url') is None:
+        # realizeaza cerere la nod
+        result = await method(url)
+        redis_db.setex('url', 60000, result)
+    else:
+        result = redis_db.get('url')
+        return result
