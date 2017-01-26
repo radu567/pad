01249bef443ca08aab5c7d1b49cc785373c4e335
radu567 <filipesco@mail.ru>
2017-01-26 15:21:45 +0200

    Update start_server.py
    Adaugata pornirea simultana a nodurilor informationale pe cele 3 porturi

--- start_server.py
+++ start_server.py
@@ -1,4 +1,29 @@
 from datawarehouse.server import main
+from threading import Thread
 
-if __name__ == '__main__':
-    main(port_server=8888)
+
+class nod():
+    def __init__(self, port):
+        self.port=port
+        self.thread1 = Thread(target=self.pornire)
+
+    def pornire(self):
+        if __name__ == '__main__':
+            main(port_server=self.port)
+
+    def run(self):
+        self.thread1.start()
+
+    def stop(self):
+        self.thread1.join()
+
+nod1 = nod(7777)
+nod2 = nod(8888)
+nod3 = nod(9999)
+
+nodes = [nod1, nod2, nod3]
+
+for node in nodes:
+    node.run()
+for node in nodes:
+    node.stop()
